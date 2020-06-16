8a22973debc19bcba4dfd46f415a423671c01759
PTR <peter.pataki@melkweg.hu>
2020-06-16 14:06:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function initMap () {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 43.165,
+            lng: -79.383,
+        },
+        zoom: 10,
+    });
+}
